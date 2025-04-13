b4d58ffb3e980f01997cfb38ded607aa571016e6
shreyashdhas0604 <shreyashrdhas@gmail.com>
2025-04-13 19:50:10 +0530

    implement breadth-first search algorithm

---
+++ graph/bfs.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bfs(int start, vector<bool>& visited, vector<vector<int>>& adj) {
+    queue<int> q;
+    q.push(start);
+    visited[start] = true;
+
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+        cout << node << " ";
+
+        for (int neighbor : adj[node]) {
+            if (!visited[neighbor]) {
+                visited[neighbor] = true;
+                q.push(neighbor);
+            }
+        }
+    }
+}
